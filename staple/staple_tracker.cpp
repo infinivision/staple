c7d0a58d854d12213f3cdc31b78d09bf34269d9d
loadwiki <chutianqianli@gmail.com>
2018-06-13 14:08:20 +0800

    eliminate cout

--- staple/staple_tracker.cpp
+++ staple/staple_tracker.cpp
@@ -123,8 +123,8 @@ void STAPLE_TRACKER::initializeAllAreas(const cv::Mat &im)
     fg_area.width = fg_area.width + (bg_area.width - fg_area.width) % 2;
     fg_area.height = fg_area.height + (bg_area.height - fg_area.width) % 2;
 
-    std::cout << "bg_area.width " << bg_area.width << " bg_area.height " << bg_area.height << std::endl;
-    std::cout << "fg_area.width " << fg_area.width << " fg_area.height " << fg_area.height << std::endl;
+    // std::cout << "bg_area.width " << bg_area.width << " bg_area.height " << bg_area.height << std::endl;
+    // std::cout << "fg_area.width " << fg_area.width << " fg_area.height " << fg_area.height << std::endl;
 
     // Compute the rectangle with (or close to) params.fixedArea
     // and same aspect ratio as the target bbox
@@ -135,7 +135,7 @@ void STAPLE_TRACKER::initializeAllAreas(const cv::Mat &im)
     //norm_bg_area.width  = cfg.fftw_window_len * cfg.hog_cell_size;
     //norm_bg_area.height = cfg.fftw_window_len * cfg.hog_cell_size;
 
-    std::cout << "area_resize_factor " << area_resize_factor << " norm_bg_area.width " << norm_bg_area.width << " norm_bg_area.height " << norm_bg_area.height << std::endl;
+    // std::cout << "area_resize_factor " << area_resize_factor << " norm_bg_area.width " << norm_bg_area.width << " norm_bg_area.height " << norm_bg_area.height << std::endl;
 
     // Correlation Filter (HOG) feature space
     // It smaller that the norm bg area if HOG cell size is > 1
@@ -151,7 +151,7 @@ void STAPLE_TRACKER::initializeAllAreas(const cv::Mat &im)
     norm_target_sz.width = round(norm_target_sz_w);
     norm_target_sz.height = round(norm_target_sz_h);
 
-    std::cout << "norm_target_sz.width " << norm_target_sz.width << " norm_target_sz.height " << norm_target_sz.height << std::endl;
+    // std::cout << "norm_target_sz.width " << norm_target_sz.width << " norm_target_sz.height " << norm_target_sz.height << std::endl;
 
     // distance (on one side) between target and bg area
     cv::Size norm_pad;
@@ -171,7 +171,7 @@ void STAPLE_TRACKER::initializeAllAreas(const cv::Mat &im)
     norm_pwp_search_area.width = norm_target_sz.width + norm_delta_area.width - 1;
     norm_pwp_search_area.height = norm_target_sz.height + norm_delta_area.height - 1;
 
-    std::cout << "norm_pwp_search_area.width " << norm_pwp_search_area.width << " norm_pwp_search_area.height " << norm_pwp_search_area.height << std::endl;
+    // std::cout << "norm_pwp_search_area.width " << norm_pwp_search_area.width << " norm_pwp_search_area.height " << norm_pwp_search_area.height << std::endl;
 }
 
 // GET_SUBWINDOW Obtain image sub-window, padding is done by replicating border values.
@@ -449,7 +449,7 @@ void * fftwf_mallocWrapper(size_t n) {
 void inline STAPLE_TRACKER::fftwInit(int row, int col, int cn) {
 
     // add for eigen fftw
-    cout << "fft compute windows row col: "<<row << "*"<< col << endl;
+    // cout << "fft compute windows row col: "<<row << "*"<< col << endl;
     y_fftw  = cv::Mat(row,col,CV_32FC1,fftwf_mallocWrapper(sizeof (float) * row * col ));
     yf_fftw = cv::Mat(row,col/2+1,CV_32FC2,fftwf_mallocWrapper(sizeof (fftwf_complex) * row * (col/2+1) ));
 
